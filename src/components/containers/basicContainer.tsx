9e0e4b64f2529fab2f52cede4b691701005c968f
do0x0ob <do0x0ob@protonmail.com>
2024-06-03 17:18:28 +0800

    ****

--- src/components/containers/basicContainer.tsx
+++ src/components/containers/basicContainer.tsx
@@ -289,7 +289,7 @@ const BasicContainer = () => {
   }
 
 
-  // Accept Task
+  // Accept Task TODO: here FIXME:
   const handleAcceptTask = async (selectedTask: Task) => {
     if (!account) {
       toast.error("Please connect your wallet");

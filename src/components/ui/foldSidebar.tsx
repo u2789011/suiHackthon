a6529cb453cfe71355eb1f5de145434583f8b9ac
do0x0ob <do0x0ob@protonmail.com>
2024-06-19 14:11:18 +0800

    - fix default sidebar width when no suifrens found

--- src/components/ui/foldSidebar.tsx
+++ src/components/ui/foldSidebar.tsx
@@ -24,7 +24,7 @@ const SuifrenCard2: React.FC<SuifrenCard2Props> = ({ suiFrenSvg, isError }) => {
       <button
         onClick={() => setIsOpen(!isOpen)}
         className={`fixed top-1/2 transform -translate-y-1/2 bg-orange-400 bg-opacity-95 text-white text-center px-2 py-1 rounded-r-xl transition-transform duration-360 ease-in-out ${
-          isOpen ? 'left-64' : 'left-0'
+          isOpen ? 'left-70' : 'left-0'
         }`}
         style={{ zIndex: 1001 }}
       >
@@ -33,20 +33,20 @@ const SuifrenCard2: React.FC<SuifrenCard2Props> = ({ suiFrenSvg, isError }) => {
       <div
         className={`fixed left-0 transform ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-360 ease-in-out w-70 backdrop-blur-lg text-white p-4 shadow-lg rounded-r-4xl`}
+        } transition-transform duration-360 ease-in-out w-72 backdrop-blur-lg text-white p-4 shadow-lg rounded-r-4xl`}
         style={{ minHeight: '480px', maxHeight: '90vh', top: '50%', transform: `translateY(-50%) ${isOpen ? 'translateX(0)' : 'translateX(-100%)'}`, zIndex: 1000 }}
       >
-        <div className="sui-fren-container overflow-auto">
+        <div className="sui-fren-container overflow-auto w-full">
           {isError ? (
-            <>
+            <div className="w-full flex flex-col items-center">
               <Avatar
                 src={defaultSvgPath}
                 alt="Void Fren"
                 size="lg"
                 className="mb-4 mx-auto rounded-full w-32 h-32"
               />
-              <h3 className="font-sans">No SuiFren Yet</h3>
-              <Button size="sm">
+              <h3 className="font-sans text-center">No SuiFren Yet</h3>
+              <Button size="sm" className="mx-auto">
                 <Link
                   href="https://suifrens-testnet-az1ho1jq2-mysten-labs.vercel.app/mint"
                   isExternal
@@ -56,9 +56,9 @@ const SuifrenCard2: React.FC<SuifrenCard2Props> = ({ suiFrenSvg, isError }) => {
                   Go Get A SuiFren
                 </Link>
               </Button>
-            </>
+            </div>
           ) : (
-            <div>
+            <div className="w-full">
               <Accordion keepContentMounted={true} defaultExpandedKeys={["1"]}>
                 <AccordionItem
                   key="1"

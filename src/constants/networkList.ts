cd70cbdad6d7e3d87359c00f4409f1dc573408f6
do0x0ob <do0x0ob@protonmail.com>
2024-06-28 04:29:03 +0800

    - add todo comments

--- src/constants/networkList.ts
+++ src/constants/networkList.ts
@@ -8,4 +8,4 @@ export const TASK_MANAGER_ID =
     //"0xd879ec6a7d5388d7ebffaea174827837ec1833f39137be2decad50dcc66139f0"; // Devnet
     "0x72870ade30d601c9367d22813940d5b584205b360307a4a65d268dbf12e31bbb"; // Testnet
 
-export const PUBLISH_PUBLIC_TASK_ITEM_NAME = "magic wand"
+export const PUBLISH_PUBLIC_TASK_ITEM_NAME = "magic wand" //TODO: change this to customize series
